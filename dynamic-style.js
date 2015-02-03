71695cdc85b875b6f0d506439333dd1385d6c1a9
Michael Trestman <michael.a.trestman@gmail.com>
2015-02-03 10:22:23 -0800

    adds another trait

--- dynamic-style.js
+++ dynamic-style.js
@@ -4,7 +4,8 @@ var colors = {
   conscious: "rgba(201, 181, 59, 0.4)",
   homeothermy: "rgba(200, 10, 40, 0.4)",
   flight: "rgba(22, 50, 200, 0.4)",
-  'non-aquatic': "rgba(22, 250, 100, 0.7)"
+  'non-aquatic': "rgba(22, 250, 100, 0.7)",
+  'secondary-aquatic': 'rgba(122, 150, 00, 0.7)'
 }
 
 var OriginHypotheses = {
